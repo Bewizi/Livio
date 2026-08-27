7976f597fc6b785b193173a9999ff8508b328136
Toluwalase Bewaji <79145995+Bewizi@users.noreply.github.com>
2026-08-27 12:25:11 +0100

    feat: Created a reusable floating button to be used in MessageView and Properties View

---
+++ Livio/Features/MessageScreen/Presentation/Components/MessageRow.swift
@@ -0,0 +1,18 @@
+//
+//  MessageRow.swift
+//  Livio
+//
+//  Created by Toluwalase on 27/08/2026.
+//
+
+import SwiftUI
+
+struct MessageRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageRow()
+}
